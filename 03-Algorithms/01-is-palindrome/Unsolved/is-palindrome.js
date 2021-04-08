513aee001efd7055e418125faf99e6613985feaa
Arpita Saha <arpita.sahaa@gmail.com>
2021-04-08 09:06:37 -0400

    algo unsolved exercise 01 and 02

--- 03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 03-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,35 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+// var isPalindrome = function(str) {};
+
+const isPalindrome = (str)=> {
+    let reverse = str.split("").reverse().join("");
+    if(reverse.toLowerCase() === str.toLowerCase()){
+        return true
+    }else {
+        return false
+    }
+}
+
+let result = isPalindrome("Madam");
+console.log(result);
+
+
+// option - 2
+// const isPalindrome = (str)=> {
+//     let reverseStr ="";
+//     for (i=str.length-1; i>=0; i--){
+//         reverseStr = reverseStr + str[i]
+//     }
+//     console.log (reverseStr);
+
+//     if(reverseStr === str){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// };
+
+// let result = isPalindrome("arpita");
+// console.log(result);
--- 03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,15 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+// var factorial = function(num) {};
+
+const factorial = (num) => {
+  let result = 1;
+
+  for (i = 1; i <= num; i++) {
+    result = result * i;
+  }
+
+  console.log(result);
+};
+
+factorial(5);
